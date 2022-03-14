ce343dbff5526691be6d4285c944c3ef399cac77
Mike Radcliffe <mikerad83@gmail.com>
2022-03-14 12:03:23 -0400

    Added first pass at pseudocode comments. Logic required for the task has not been fully worke out at this point.

---
+++ script.js
@@ -0,0 +1,22 @@
+//  0) Declare variables such as 'cpuAns', 'userAns', and any others as needed.
+
+//  1) CPU generates 'Rock', 'Paper', or 'Scissors' at random
+
+//  2) Prompt user to enter 'Rock', 'Paper, or 'Scissors'. Make this field case insensitive.
+
+//  3) Compare cpuAns and userAns.
+
+//      if cpuAns = userAns, return 'Draw'
+
+//      else if cpuAns = 'Rock' & userAns = 'Paper', return 'User Wins'
+//      else if userAns = 'Scissors', return 'CPU Wins'
+
+//      else if cpuAns = 'Paper' & userAns = 'Scissors', return 'User Wins'
+//      else if userAns = 'Rock', return 'CPU Wins'
+
+//      else if cpuAns = 'Scissors' & userAns = 'Rock', return 'User Wins'
+//      else if userAns = 'Paper', return 'CPU Wins'
+
+//  4) Tally results
+
+//  5) Compare results
